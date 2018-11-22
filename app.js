926698d87b8a2203c8f39ffe58ab9bcd5a32df34
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2018-11-22 12:01:01 -0400

    Added New Campground post route & new campground form

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 let campgrounds = [
   {name: "Salmon Creek", image:"https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f2c97ca5e4b0b0_340.jpg"},
@@ -13,6 +16,16 @@ app.get("/", (req, res) => res.render("landing"));
 
 app.get("/campgrounds", (req, res) => {
   res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.get("/campgrounds/new", (req, res) => res.render("new"));
+
+app.post("/campgrounds", (req, res) => {
+  let name = req.body.name;
+  let image = req.body.image;
+  let newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
 });
 
 app.listen(port, () => console.log(`We are live on port ${port}`));
