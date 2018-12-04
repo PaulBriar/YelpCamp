58838a6d0bd02f113d137330e51a66eca9d8b8df
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2018-12-04 12:57:31 -0400

    Set up user associations with comments & campgrounds

--- models/campground.js
+++ models/campground.js
@@ -5,12 +5,19 @@ let campgroundSchema = new mongoose.Schema({
   name: String,
   image: String,
   description: String,
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    username: String
+  },
   comments: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Comment"
+      ref: "Comment",
     }
-  ]
+  ],
 });
 
 module.exports = mongoose.model("Campground", campgroundSchema);
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,14 +14,18 @@ router.get("/", (req, res) => {
 });
 
 //New Route - Show form to create new campground
-router.get("/new", (req, res) => res.render("campgrounds/new"));
+router.get("/new", isLoggedIn, (req, res) => res.render("campgrounds/new"));
 
 //Create Route - Add new campground to DB
-router.post("/campgrounds", (req, res) => {
+router.post("/", isLoggedIn, (req, res) => {
   let name = req.body.name;
   let image = req.body.image;
   let desc = req.body.description;
-  let newCampground = {name: name, image: image, description: desc};
+  let author = {
+    id: req.user._id,
+    username: req.user.username
+  };
+  let newCampground = {name: name, image: image, description: desc, author: author};
   Campground.create(newCampground, (err, newlyCreated) => {
     if(err) {
       console.log(err);
@@ -42,5 +46,13 @@ router.get("/:id", (req, res) => {
     }
   });
 });
+
+//Middleware
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
 
 module.exports = router;
