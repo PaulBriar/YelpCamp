69597a957f03e394469e8ade77d5c16ebaa76e34
paulbriar <pbriar@live.com>
2019-04-02 19:57:17 -0300

    Changed landing page button

--- app.js
+++ app.js
@@ -63,4 +63,5 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, () => console.log(`Server is live`));
+app.listen(port, () => console.log('Local is live'));
+//app.listen(process.env.PORT, process.env.IP, () => console.log(`Server is live`));
